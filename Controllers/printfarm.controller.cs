988a60650a7d463683d61eb120accf8e1d0b3795
dagmawi-asfaw <71550160+dagmawi-asfaw@users.noreply.github.com>
2024-12-16 08:32:13 +0300

    feat: enable xml comments and add comments to controller method

--- Controllers/printfarm.controller.cs
+++ Controllers/printfarm.controller.cs
@@ -8,21 +8,37 @@ public class PrintFarmController : ControllerBase
 {
     // public PrintFarmController() { }
 
-    [HttpGet]
-    public string GetPrice(int height, int width)
-    {
-        return "default get";
-    }
 
 
 
 
+    /// <summary>Calculates price based on the height and width of the frame</summary>
+    /// <remarks>
+    /// Sample request
+    ///    GET http://localhost:5148/PrintFarm/200/220
+    ///       Accept: application/json
+    /// </remarks>
+    /// <param name="height"/>
+    /// <param name="width"/>
+    /// <returns>string</returns>
+    /// <response code="200">Returns the cost of the frame in dollars.</response>
+    /// <response code="400">If the amount of frame material needed is less than 20 inches or greater than 1000 inches.</response>
     [HttpGet("{width}/{height}")]
-    public string GetIt(int height, int width)
+    [Produces("text/plain")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public ActionResult<String> GetPrice(int height, int width)
     {
         string result;
         result = CalculatePrice(Convert.ToDouble(height), Convert.ToDouble(width));
-        return $"The cost of a {height}x{width} frame is {result}";
+        if (result == "not valid")
+        {
+            return BadRequest(result);
+        }
+        else
+        {
+            return Ok($"The cost of a {height}x{width} frame is ${result}");
+        }
     }
 
     private string CalculatePrice(double height, double width)
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,27 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen((options) =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "PrintFramer API",
+        Description = "Calculates the cost of a picture frame based on its dimensions.",
+        TermsOfService = new Uri("https://go.microsoft.com/fwlink/?LinkID=206977"),
+        Contact = new OpenApiContact
+        {
+            Name = "Your name",
+            Email = string.Empty,
+            Url = new Uri("https://learn.microsoft.com/training")
+        }
+    });
+
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddOpenApi();
 
@@ -17,7 +40,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI((options) => options.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "My API V1"));
+    app.UseSwaggerUI((options) =>
+    {
+        options.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "My API V1");
+    });
 
     app.MapOpenApi();
 
